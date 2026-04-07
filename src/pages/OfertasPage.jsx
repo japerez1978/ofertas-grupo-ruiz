7654af6a10238361c83d9b5cd333973258a3e02a
Juan Angel <juanangel@MacBook-Air-de-Juan.local>
2026-04-07 12:59:59 +0200

    fix(Link): import Link from react-router-dom in OfertasPage and NegociosSinOfertaPage

--- src/pages/OfertasPage.jsx
+++ src/pages/OfertasPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useMemo, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import { getAllOfertas, writeDealScoresBatch, patchOferta, getDealStagesMap, getPresupuestadores } from '../services/hubspot'
 import { addToBacklog } from '../services/backlog'
